bdb0cd15565647815332247bfcef9af006621add
rin-kage <103043859+rin-kage@users.noreply.github.com>
2022-05-26 11:09:22 +0900

    Update memo.rb

--- memo.rb
+++ memo.rb
@@ -2,10 +2,10 @@ require "csv"
 
 puts "1(新規でメモ作成) 2(既存のメモを編集する)"
 
-memo_type = gets.to_s #１か２の入力待機
+memo_type = gets.to_i #１か２の入力待機
 
 
-if memo_type == "1"
+if memo_type == 1
   puts "拡張子を除いたファイル名を入力してください。"
   file_name = gets.chomp #ファイル名の入力待機
   
@@ -16,7 +16,7 @@ if memo_type == "1"
   CSV.open("#{file_name}.csv","w") do |csv|
     csv << ["#{input_memo}"]
 end  
-elsif memo_type == "2"
+elsif memo_type == 2
   puts "拡張子を除いたファイル名を入力してください。"
   file_name = gets.chomp
   
@@ -27,5 +27,5 @@ elsif memo_type == "2"
     csv << ["#{input_memo}"]
 end    
 else
-  puts "1(新規でメモ作成) 2(既存のメモを保存する)"
+  puts "?????"
 end
